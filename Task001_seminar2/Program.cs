5b31abac5018820dfa740e2a3a7184001bf4caca
Zunickx <Zunickx@gmail.com>
2022-08-12 22:16:51 +0300

    Task001_seminar2 (Задача на вывод второй цифры из трехзначного числа)

---
+++ Task001_seminar2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+string threeDigitNumber = (Console.ReadLine()!);
+var digitArray = threeDigitNumber.Select(digit => int.Parse(digit.ToString())).ToArray(); // передали массиву строку полученную от Пользователя (число введенное Пользователем)
+// и преобразовали ее в число, а затем вложили в массив
+Console.WriteLine(digitArray[1]);
