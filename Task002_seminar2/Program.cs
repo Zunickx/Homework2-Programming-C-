19e81186b0e43d5b8235f30c665b1833f900250b
Zunickx <Zunickx@gmail.com>
2022-08-13 20:43:18 +0300

    Task001_seminar2 (Задача на вывод третьей цифры любого числа

---
+++ Task002_seminar2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+string threeDigitNumber = (Console.ReadLine()!);
+var digitArray = threeDigitNumber.Select(digit => int.Parse(digit.ToString())).ToArray(); // передали массиву строку полученную от Пользователя (число введенное Пользователем)
+// и преобразовали ее в число, а затем вложили в массив
+
+if(digitArray.Length >= 3)
+{
+    Console.WriteLine(digitArray[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
