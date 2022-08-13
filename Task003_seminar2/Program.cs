60f97ff2b65b223ae5c01e9fdfe6091f1f7348c2
Zunickx <Zunickx@gmail.com>
2022-08-13 22:27:06 +0300

    Task003_seminar2 (Задача на определение дня недели)

---
+++ Task003_seminar2/Program.cs
@@ -0,0 +1,25 @@
+﻿string dayOfWeek = "";
+int dayX;    
+Console.WriteLine("Введите число, обозначающее день недели:");
+    do
+    {
+        dayOfWeek = (Console.ReadLine()!);
+        dayX = int.Parse(dayOfWeek);
+        if((dayX >= 1) && (dayX <=5))
+            {
+                Console.WriteLine("Это будни");
+            }
+        else if(dayX == 6 || dayX == 7)
+            {
+                Console.WriteLine("Да, это выходной!");
+            }
+        /*else if(dayX == 7)
+            {
+                Console.WriteLine("Да, это выходной!");
+            }*/
+        else
+            {
+                Console.WriteLine("Нет такого дня недели! Введите число от 1 до 7:");
+            }
+    } while((dayX < 1) || (dayX > 7));
+
